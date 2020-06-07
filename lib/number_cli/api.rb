2a64255450a76be441d4ca5821fba06d757cf097
Romario Gustave <romagustave10@gmail.com>
2020-06-07 01:33:03 +0000

    almost final

--- lib/number_cli/api.rb
+++ lib/number_cli/api.rb
@@ -3,15 +3,14 @@ class Api
   BASE_URL = "http://numbersapi.com/1..100"
 
   def self.get_number
-    #binding.pry
+
     res = RestClient.get(BASE_URL)
     data = JSON.parse(res.body)
 
     data.each do |number|
-      #binding.pry
       fact = number[1]
       id = number[0].to_i
-#binding.pry
+
       Number.new(fact, id)
     end
   end
--- lib/number_cli/cli.rb
+++ lib/number_cli/cli.rb
@@ -1,10 +1,16 @@
 class Cli
 
   def run
+    # Program guide
     welcome
     Api.get_number
     menu
 
+  end
+
+  def welcome
+    # Displays a welcome message to the user
+    puts "Welcome to my number Cli"
   end
 
   def menu
@@ -18,22 +24,13 @@ class Cli
 
   end
 
-  #def print_all
-    #print all output
-    #Number.all.each{|n| puts "#{n.id}.) #{n.fact}"}
-  #end
-
   def print_selection
     #asking the user for a valid input
     puts "Choose a number from 1 to 100 for a random fact"
   end
 
-  def error
-    #determine if iput is in input range if not it will display this message
-    puts "Invalid entry"
-  end
-
   def input
+    # Registers the input from the user
     gets.chomp
   end
 
@@ -45,47 +42,51 @@ class Cli
       sleep 1
       menu
     end
-
+    # It will display the id
     id
 
   end
 
+  def error
+    #determine if iput is in input range if not it will display this message
+    puts "Invalid entry"
+  end
+
+  def get_number_fact(id)
+    # Will get the fact from the user via chosen number
+    num = Number.find_by_id(id)
+    num
+  end
+
+  def print_fact(id)
+    # It will print the facts
+    puts "#{id.fact}"
+  end
+
+  def print_continue
+    # Interacting with the user asking whether or not to continue
+    puts 'Do you want to try again y/n'
+  end
+
+  def choice
+    # Gets the user choice
+    gets.chomp
+  end
+
   def continue?(choice)
+    # If the user typed 'y' it will return to menue
     if choice == 'y'
       menu
     else
+      # If not, the good by method will be called
       print_goodbye
       exit
 
     end
-
   end
 
-  def welcome
-    puts "Welcome to my number Cli"
+  def print_goodbye
+    # Prints a goodbye message
+    puts "Thanks see you again soon"
   end
-
-  def get_number_fact(id)
-   num = Number.find_by_id(id)
-   num
-end
-
-def print_continue
-  puts 'Do you want to try again y/n'
-end
-
-def print_goodbye
-  puts "Thanks see you again soon"
-end
-
-def choice
-  gets.chomp
-end
-def print_fact(id)
-  #binding.pry
-  puts "#{id.fact}"
-
-
-end
-
 end
